7aa0eb240c3ef804d53db27452ad7146fb0a7c6c
Dale Han <moordo91@gmail.com>
2023-04-19 11:07:36 +0900

    23-04-19

--- birdex.py
+++ birdex.py
@@ -1,10 +1,24 @@
-﻿from flask import Flask, render_template
-import pandas as pd
+﻿from flask import Flask, render_template, request
 import database as db
+from datetime import datetime
 
-birdex = pd.DataFrame(db.rows)
+def birdex_content():
+    return """
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Document</title>
+        </head>
+        <body>
+            
+        </body>
+        </html>
+"""
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def index():
@@ -16,7 +30,23 @@ def capture():
 
 @app.route('/birdex/')
 def birdex():
-    return render_template('birdex_list.html')
+    df = db.get_switch()
+    switch_frame = str(df.to_json())
+    return render_template('birdex_list.html', switch_frame=switch_frame)
+
+@app.route("/send_index", methods = ["post"])
+def update():
+    getIndex = request.get_json() + 1 # 모델 클래스에 비해 ???이 더해졌으므로 +1
+    date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    sql = f"UPDATE `birdex`.`birdex` SET `date` = '{date}', `get_switch` = '1' WHERE (`no` = '{getIndex}');"
+    db.update(sql)
+    return "TODO"
+    
+@app.route('/birdex/content/')
+def birdex_content():
+    df = db.get_switch()
+    switch_frame = df.to_json()
+    return render_template('birdex_content.html', switch_frame=switch_frame)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- database.py
+++ database.py
@@ -1,12 +1,28 @@
 import pymysql
+import pandas as pd
 
-con = pymysql.connect(host='localhost', user='root', password='0000',
-                      db='birdex', charset='utf8',
-                      autocommit=True,
-                      cursorclass=pymysql.cursors.DictCursor
-                     )
-cur = con.cursor()
-sql = "SELECT * FROM birdex"
-cur.execute(sql)
-rows = cur.fetchall()
-con.close()
+def update(sql):
+    con = pymysql.connect(host='localhost', user='root', password='0000',
+                        db='birdex', charset='utf8',
+                        autocommit=True,
+                        cursorclass=pymysql.cursors.DictCursor
+                        )
+    cur = con.cursor()
+    cur.execute(sql)
+    con.close()
+    
+def get_switch():
+    con = pymysql.connect(host='localhost', user='root', password='0000',
+                        db='birdex', charset='utf8',
+                        autocommit=True,
+                        cursorclass=pymysql.cursors.DictCursor
+                        )
+    cursor = con.cursor()
+    sql = "SELECT * FROM birdex"
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    df = pd.DataFrame(result)
+    data = df['get_switch']
+    print(data)
+    con.close()
+    return data
